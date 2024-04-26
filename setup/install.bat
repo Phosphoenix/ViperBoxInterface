eb3d346358641bbc396071d3fddb35446188026a
sbalk <stijn.balk@gmail.com>
2024-04-26 12:14:01 +0200

    rename dev to main

--- setup/install.bat
+++ setup/install.bat
@@ -1,3 +1,3 @@
 @echo off
-SET SCRIPT_URL=https://raw.githubusercontent.com/sbalk/viperboxinterface/dev/setup/download_script.ps1
+SET SCRIPT_URL=https://raw.githubusercontent.com/sbalk/viperboxinterface/main/setup/download_script.ps1
 powershell -Command "& { iwr -useb %SCRIPT_URL% | iex; }"
--- setup/update.ps1
+++ setup/update.ps1
@@ -2,22 +2,22 @@ $fileList = @("main.py", "gui.py", "api_classes.py", "XML_handler.py", "ViperBox
 $scriptPath = Split-Path -Parent $MyInvocation.MyCommand.Path
 $mainFolderPath = Split-Path -Parent $scriptPath
 foreach ($file in $fileList) {
-    $fileUrl = "https://raw.githubusercontent.com/sbalk/viperboxinterface/dev/$file"
+    $fileUrl = "https://raw.githubusercontent.com/sbalk/viperboxinterface/main/$file"
     $fileDestination = Join-Path -Path $mainFolderPath -ChildPath $file
     Invoke-WebRequest -Uri $fileUrl -OutFile $fileDestination
 }
 # defaults.py
-$fileUrl = "https://raw.githubusercontent.com/sbalk/viperboxinterface/dev/defaults/defaults.py"
+$fileUrl = "https://raw.githubusercontent.com/sbalk/viperboxinterface/main/defaults/defaults.py"
 $fileDestination = Join-Path -Path $mainFolderPath -ChildPath "defaults/defaults.py"
 Invoke-WebRequest -Uri $fileUrl -OutFile $fileDestination
 
 # update.ps1
-$fileUrl = "https://raw.githubusercontent.com/sbalk/viperboxinterface/dev/setup/update.ps1"
+$fileUrl = "https://raw.githubusercontent.com/sbalk/viperboxinterface/main/setup/update.ps1"
 $fileDestination = Join-Path -Path $mainFolderPath -ChildPath "setup/update.ps1"
 Invoke-WebRequest -Uri $fileUrl -OutFile $fileDestination
 
 # viperbox.yaml
-$fileUrl = "https://raw.githubusercontent.com/sbalk/viperboxinterface/dev/setup/viperbox.yaml"
+$fileUrl = "https://raw.githubusercontent.com/sbalk/viperboxinterface/main/setup/viperbox.yaml"
 $fileDestination = Join-Path -Path $mainFolderPath -ChildPath "setup/viperbox.yaml"
 Invoke-WebRequest -Uri $fileUrl -OutFile $fileDestination
 
